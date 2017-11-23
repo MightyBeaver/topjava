b62d96fb7335e6f33458c4b8b5954b498dc64eb0
MightyBeaver <bobrnah486@gmail.com>
2017-11-23 12:36:44 +0300

    HW3 fix

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -28,9 +28,10 @@ CREATE TABLE user_roles
 CREATE TABLE meals
 (
   id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  user_id          INTEGER                  NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+  user_id          INTEGER                  NOT NULL,
   date_time        TIMESTAMP DEFAULT now()  NOT NULL,
   description      VARCHAR                  NOT NULL,
-  calories         INTEGER                  NOT NULL
+  calories         INTEGER                  NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX date_time_idx ON meals (user_id,date_time);
--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -43,7 +43,7 @@ public class MealTestData {
             "Админ обед", 1000);
 
     public static void assertMatch(Meal actual, Meal expected) {
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).isEqualToComparingFieldByField(expected);
     }
 
     public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
@@ -51,6 +51,6 @@ public class MealTestData {
     }
 
     public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
-        assertThat(actual).isEqualTo(expected);
+        assertThat(actual).usingFieldByFieldElementComparator().isEqualTo(expected);
     }
 }
